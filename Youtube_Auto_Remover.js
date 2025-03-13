bf9abfe12b945ddb9aba944b2049e7d740212937
Tim Hyde <100077379+SON-OF-EDDY@users.noreply.github.com>
2025-03-14 01:58:15 +0300

    Update Youtube_Auto_Remover.js

--- Youtube_Auto_Remover.js
+++ Youtube_Auto_Remover.js
@@ -8,35 +8,34 @@ function delay(ms) {
 }
 
 async function removeVideos() {
-    
     for (let i = 0; i < numberOfTimesToExecute; i++) {
-        
-        let dropdownMenus = document.querySelectorAll('yt-icon-button#button.dropdown-trigger.style-scope.ytd-menu-renderer');
+        for (let j = 0; j < 2; j++) {
+            let dropdownMenus = document.querySelectorAll('yt-icon-button#button.dropdown-trigger.style-scope.ytd-menu-renderer');
 
-        console.log("Dropdown buttons found:", dropdownMenus);
-        console.log("Number of dropdown menus:", dropdownMenus.length);
+            console.log("Dropdown buttons found:", dropdownMenus);
+            console.log("Number of dropdown menus:", dropdownMenus.length);
 
-        let firstVideoMenu = dropdownMenus[2];
+            let firstVideoMenu = dropdownMenus[2];
 
-        // Simulate the click to open the dropdown menu
-        firstVideoMenu.click();
-        console.log(`firstVideoMenu clicked!`);
+            // Simulate the click to open the dropdown menu
+            firstVideoMenu.click();
+            console.log(`firstVideoMenu clicked!`);
 
-        let revealedButtons = document.querySelectorAll('tp-yt-paper-item[role="option"][aria-disabled="false"] yt-formatted-string');
-        console.log(revealedButtons);
+            let revealedButtons = document.querySelectorAll('tp-yt-paper-item[role="option"][aria-disabled="false"] yt-formatted-string');
+            console.log(revealedButtons);
 
-        let deleteButton = revealedButtons[2];
+            let deleteButton = revealedButtons[2];
 
-        // Find the closest tp-yt-paper-item parent of the third button
-        let buttonToClick = deleteButton.closest('tp-yt-paper-item');
+            // Find the closest tp-yt-paper-item parent of the third button
+            let buttonToClick = deleteButton.closest('tp-yt-paper-item');
 
-        // Simulate the click to remove the video
-        buttonToClick.click();
+            // Simulate the click to remove the video
+            buttonToClick.click();
 
-        // Add a delay to prevent rate-limiting
-        await delay(1500); // Adjust this value (1500ms = 1.5 seconds)
+            // Add a delay to prevent rate-limiting
+            await delay(1500); // Adjust this value (1500ms = 1.5 seconds)
+        }
     }
-    
 }
 
 // Start the process

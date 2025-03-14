208d67947186829e1a4cc5965866bc3f7a1a155b
Tim Hyde <100077379+SON-OF-EDDY@users.noreply.github.com>
2025-03-14 03:02:29 +0300

    Update Youtube_Auto_Remover.js

--- Youtube_Auto_Remover.js
+++ Youtube_Auto_Remover.js
@@ -1,10 +1,16 @@
-//Might have to copy paste run it a few times.
-// It can just stop after a few tens of videos too.
+// Might have to "copy paste run" it a few times first in the console to "get it going".
+// It can just stop after a few tens of videos too. So you'll have to restart it- still better than doing it manually but...
+
+
+// BASED ON THE PLAYLIST YOU WANT TO AUTO-DELETE CHANGE THE LINE BELOW ACCORDINGLY!!!
+// let deleteButton = revealedButtons[6]; - it's for LIKED VIDEOS
+// let deleteButton = revealedButtons[2]; - it's for WATCHED VIDEOS
+
 
 let dropdownMenus = document.querySelectorAll('yt-icon-button#button.dropdown-trigger.style-scope.ytd-menu-renderer');
 
 let numberOfTimesToExecute = dropdownMenus.length;
-console.log(numberOfTimesToExecute);
+//console.log(numberOfTimesToExecute);
 
 function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -15,22 +21,22 @@ async function removeVideos() {
         for (let j = 0; j < 2; j++) {
             let dropdownMenus = document.querySelectorAll('yt-icon-button#button.dropdown-trigger.style-scope.ytd-menu-renderer');
 
-            console.log("Dropdown buttons found:", dropdownMenus);
-            console.log("Number of dropdown menus:", dropdownMenus.length);
+            //console.log("Dropdown buttons found:", dropdownMenus);
+            //console.log("Number of dropdown menus:", dropdownMenus.length);
 
             let firstVideoMenu = dropdownMenus[2];
 
             // Simulate the click to open the dropdown menu
             firstVideoMenu.click();
-            console.log(`firstVideoMenu clicked!`);
+            //console.log(`firstVideoMenu clicked!`);
 
             let revealedButtons = document.querySelectorAll('tp-yt-paper-item[role="option"][aria-disabled="false"] yt-formatted-string');
-            console.log(revealedButtons);
+            //console.log(revealedButtons);
 
             let deleteButton = revealedButtons[2];
 
-            // Find the closest tp-yt-paper-item parent of the third button
-            let buttonToClick = deleteButton.closest('tp-yt-paper-item');
+            //let buttonToClick = deleteButton.closest('tp-yt-paper-item');
+            let buttonToClick = deleteButton.parentElement;
 
             // Simulate the click to remove the video
             buttonToClick.click();
